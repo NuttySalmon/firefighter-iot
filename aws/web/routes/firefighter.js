341876db1525ad1055f5456deeca417f2ce08af2
NuttySalmon <nuttysalmon@gmail.com>
2019-11-30 14:56:43 -0800

    Added current value and "no data" indication.

--- aws/web/routes/firefighter.js
+++ aws/web/routes/firefighter.js
@@ -7,14 +7,14 @@ router.get('/:id', (req, res) => {
         title: fighterName + 'stats',
         id: fighterName,
         fields: [
-            {name: 'Temperature', tag:'temp'},
-            {name: 'Humidity', tag:'hum'},
-            {name: 'Pressure', tag:'pres'},
-            {name: 'O2 Level', tag:'o2'},
-            {name: 'CO concentration', tag:'co'},
-            {name: 'HCN concentration', tag:'hcn'},
-            {name: 'Heart rate', tag:'heart'},
-            {name: 'Movement', tag:'mov'}
+            {name: 'Temperature', tag:'temp', unit:'&#176;C'},
+            {name: 'Humidity', tag:'hum', unit: '&#37;'},
+            {name: 'Pressure', tag:'pres', unit: 'Pa'},
+            {name: 'O2 Level', tag:'o2', unit: '%'},
+            {name: 'CO concentration', tag:'co', unit: 'ppm'},
+            {name: 'HCN concentration', tag:'hcn', unit: 'ppm'},
+            {name: 'Heart rate', tag:'heart', unit: 'bpm'},
+            {name: 'Movement', tag:'mov', unit: ''}
         ]
     });
 });
