e0a8620611acf8e2d1c89f96d7f37bf651784ef9
NuttySalmon <nuttysalmon@gmail.com>
2019-12-01 03:36:41 -0800

    Added redirection.

--- aws/web/routes/index.js
+++ aws/web/routes/index.js
@@ -2,6 +2,11 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', (req, res) => {
+  res.redirect('home/map');
+});
+
+
+router.get('/home/map', (req, res) => {
     var fighterName = req.params.name;
     res.render('index', {
       title: 'Firefighter Tracker',
