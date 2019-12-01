c7bd003dd3be5e0f33d97e511e8ad3ab85e4c434
NuttySalmon <nuttysalmon@gmail.com>
2019-12-01 13:49:36 -0800

    Removed Google API key.

--- aws/web/config.js
+++ aws/web/config.js
@@ -1,6 +1,6 @@
 const s3Name = 'firefighter-web-assets';  // use unique name
-const googleMapsAPIKey = 'AIzaSyCf0GTb9p4ksFKrcs_fms33bIb_AEniq7I';  // change to your own 
-const googleMapsAPIKeyLocal = 'AIzaSyCdxcUu_caB5cLs82rZbAZiiFJscFlDwXk';
+const googleMapsAPIKey = '';  // add your own api key 
+const googleMapsAPIKeyLocal = ''; // api key for local testing
 module.exports.s3Name = () => {return s3Name}
 module.exports.staticPath = 'http://' + s3Name + '.s3-website-us-east-1.amazonaws.com/'
 module.exports.googleMapsAPIKey = googleMapsAPIKey;
