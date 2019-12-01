64a6658d401bf25a522b94369722a419225acd97
NuttySalmon <nuttysalmon@gmail.com>
2019-12-01 13:22:25 -0800

    Refracted frontend javascript code to js files.

--- aws/static/js/chart.js
+++ aws/static/js/chart.js
@@ -1,126 +1,98 @@
-const HEART_COLOR = "#FF0C0C";
-const HUM_COLOR = "#009CFF";
-const TEMP_COLOR = "#FF8400";
-const PRES_COLOR = "#FFFFFF";
-const CO_COLOR = "#FFFFFF";
-const O2_COLOR = "#FFFFFF";
-const HCN_COLOR = "#FFFFFF";
-const MOV_COLOR = "#FFFFFF";
+const COLORS = {
+  heart: "#FF0C0C",
+  hum: "#009CFF",
+  temp: "#FF8400",
+  pres: "#FFFFFF",
+  co: "#FFFFFF",
+  o2: "#FFFFFF",
+  hcn: "#FFFFFF",
+  mov: "#FFFFFF"
+}
 
+// for mock data
+const testDate = "2019-12-01T08:30:00"
+var data = [];
+var date = new Date(testDate);
+date.setMilliseconds(0)
+date -= 2000;
 
-function initGraph(customColorStr, graphId, initData, verDist) {
-  let customColor = am4core.color(customColorStr);
-  // Themes begin
-  am4core.useTheme(am4themes_dark);
-  am4core.useTheme(am4themes_animated);
-  // Themes end
+/**
+Get past data to initialize graph
+*/
+function getHistory(id, dataPoints, callback){
+  let url = '../api/firefighter/history';
+  let payload = {
+    'id' : id,
+    'count': dataPoints,
+    'datetime': date
+  };
+  getData(url, payload, callback);
+  return data;
+}
 
-  let chart = am4core.create(graphId, am4charts.XYChart);
-  chart.hiddenState.properties.opacity = 0;
-
-  chart.padding(0, 0, 0, 0);
-
-  chart.zoomOutButton.disabled = true;
-
-  var data = initData;
-
-  chart.data = data;
-
-  var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-  dateAxis.renderer.grid.template.location = 0;
-  dateAxis.renderer.minGridDistance = 30;
-  dateAxis.dateFormats.setKey("second", "ss");
-  dateAxis.periodChangeDateFormats.setKey("second", "[bold]HH:mm");
-  dateAxis.periodChangeDateFormats.setKey("minute", "[bold]HH:mm");
-  dateAxis.periodChangeDateFormats.setKey("hour", "[bold]HH:mm");
-  dateAxis.renderer.inside = true;
-  dateAxis.renderer.axisFills.template.disabled = true;
-  dateAxis.renderer.ticks.template.disabled = true;
-
-  var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-  valueAxis.tooltip.disabled = true;
-  valueAxis.interpolationDuration = 500;
-  valueAxis.rangeChangeDuration = 500;
-
-  valueAxis.strictMinMax = false;
-  valueAxis.renderer.minGridDistance = verDist;
-  valueAxis.renderer.inside = true;
-
-  valueAxis.renderer.minLabelPosition = 0.05;
-  valueAxis.renderer.maxLabelPosition = 0.95;
-  valueAxis.renderer.axisFills.template.disabled = false;
-  valueAxis.renderer.ticks.template.disabled = false;
-
-  var line = new am4charts.LineSeries();
-  line.fill = customColor;
-
-  var series = chart.series.push(line);
-  series.dataFields.dateX = "date";
-  series.dataFields.valueY = "value";
-  series.interpolationDuration = 500;
-  series.defaultState.transitionDuration = 0;
-  series.tensionX = 0.8;
-
-  chart.events.on("datavalidated", function() {
-    dateAxis.zoom({ start: 1 / 15, end: 1.2 }, false, true);
-  });
-
-  dateAxis.interpolationDuration = 500;
-  dateAxis.rangeChangeDuration = 500;
-
-
-  // all the below is optional, makes some fancy effects
-  // gradient fill of the series
-  series.fillOpacity = 1;
-  var gradient = new am4core.LinearGradient();
-  gradient.addColor(customColor, 0);
-  gradient.addColor(customColor, 0.1);
-  series.fill = gradient;
-
-  // this makes date axis labels to fade out
-  dateAxis.renderer.labels.template.adapter.add("fillOpacity", function(fillOpacity, target) {
-    var dataItem = target.dataItem;
-    return dataItem.position;
-  })
-
-  // need to set this, otherwise fillOpacity is not changed and not set
-  dateAxis.events.on("validated", function() {
-    am4core.iter.each(dateAxis.renderer.labels.iterator(), function(label) {
-      label.fillOpacity = label.fillOpacity;
+/**
+Generate series of data for graph from bulk data
+*/
+function generateSeries(field, data){
+  let series = [];
+  data.forEach((item)=>{
+    series.push({
+      date: item.datetime,
+      value: item[field]
     })
-  })
-
-  // this makes date axis labels which are at equal minutes to be rotated
-  dateAxis.renderer.labels.template.adapter.add("rotation", function(rotation, target) {
-    var dataItem = target.dataItem;
-    if (dataItem.date && dataItem.date.getTime() == am4core.time.round(new Date(dataItem.date.getTime()), "minute").getTime()) {
-      target.verticalCenter = "middle";
-      target.horizontalCenter = "left";
-      return -90;
-    } else {
-      target.verticalCenter = "bottom";
-      target.horizontalCenter = "middle";
-      return 0;
-    }
-  })
-
-  // bullet at the front of the line
-  var bullet = series.createChild(am4charts.CircleBullet);
-  bullet.circle.radius = 3;
-  bullet.fillOpacity = 1;
-  bullet.fill = chart.colors.getIndex(0);
-  bullet.isMeasured = false;
-
-  // bullet at the front of the line
-  var bullet1 = series.bullets.push(new am4charts.CircleBullet);
-  bullet1.circle.radius = 3;
-  bullet1.fillOpacity = 0.5;
-  bullet1.fill = customColor;
-
-
-  series.events.on("validated", function() {
-    bullet.moveTo(series.dataItems.last.point);
-    bullet.validatePosition();
   });
-  return chart;
-} // end am4core.ready()
+  return series;
+}
+
+/**
+Add data to chart
+*/
+function updateData(chart, tag, value){
+  let valueQuery = '#'+ tag + '-value';
+  let overlayQuery = '#'+ tag + '-overlay'
+  if(value == undefined){
+    $(valueQuery).html('');
+    $(overlayQuery).css('display', 'block');
+  } else{
+    $(overlayQuery).css('display', 'none');
+    $(valueQuery).html(value.toFixed(2));
+    chart.addData({ date: curr, value: value}, 1);
+  }
+}
+
+/**
+Start interval to get data
+*/
+function startInterval(id, interval, callback) {
+  let i = 0;
+  let dataDatetime = date + i;
+  let payload = {
+    id: id, 
+    datetime: date + i
+  };
+  setInterval(()=>{
+    getData('../api/firefighter', payload, (data)=>{
+      callback(data, i);
+      i += interval;
+    });
+  }, interval);
+}
+
+
+/**
+Get data using ajax
+*/
+function getData(url, payload, callback){
+  $.ajax({
+    url : url,
+    type : 'GET',
+    data : payload,
+    success : (response) => {   
+       $('.overlay').css("display", "none");             
+       callback(response.data);
+    },
+    error : (request,error) => {
+      $('.overlay').css("display", "block");
+    }
+  });
+}
---
+++ aws/static/js/map.js
@@ -0,0 +1,82 @@
+var markers = {};
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: new google.maps.LatLng(38.5616539, -121.512952),
+    zoom: 12
+  });
+
+  var infoWindow = new google.maps.InfoWindow;
+  startInterval((teams) => {
+    teams.forEach((team) => {
+      var id = team.id;
+      var point = new google.maps.LatLng(
+        parseFloat(team.pos.lat),
+        parseFloat(team.pos.lng));
+      if (markers[id] === undefined) {
+        var label = id.toString() || "";
+        var marker = new google.maps.Marker({
+          title: name,
+          map: map,
+          label: label
+        });
+        zoomToFit(point);
+        markers[id] = marker;
+      }
+
+      var currMarker = markers[id];
+      //clear all listeners
+      google.maps.event.clearListeners(currMarker, 'click');
+      //popup content
+      let infowincontent = '<div style="color: black"><b><a href="../team/' + id + '">' +
+        team.name +
+        '</b><a><br/>Status: ' +
+        team.status + '<br/> Devices: ' +
+        team.membersId +
+        '</div>';
+
+
+      currMarker.setPosition(point);
+      currMarker.setIcon(icons[team.status] || icons["Deployed"]);
+      currMarker.addListener('click', function() {
+        infoWindow.setContent(infowincontent);
+        infoWindow.open(map, currMarker);
+      });
+
+
+
+    });
+  });
+}
+
+function zoomToFit(newPoint) {
+  let bounds = new google.maps.LatLngBounds();
+  for (var key in markers) {
+    bounds.extend(markers[key].position);
+  }
+  if (newPoint != null) {
+    bounds.extend(newPoint);
+  }
+  map.fitBounds(bounds);
+  if (map.getZoom() > 17)
+    map.setZoom(17);
+}
+
+function startInterval(callback) {
+  // let data;
+  var date = new Date();
+  interval = setInterval(() => {
+    $.ajax({
+      url: '../api/teams',
+      type: 'GET',
+      success: (response) => {
+        $('.overlay').css('display', 'none');
+        callback(response.data);
+      },
+      error: (request, error) => {
+        $('.overlay').css('display', 'block');
+      }
+    });
+  }, 1000);
+}
