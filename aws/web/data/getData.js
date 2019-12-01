807498322bed6ce8b2c9f64eff6c10cc941f3fc0
NuttySalmon <nuttysalmon@gmail.com>
2019-11-30 21:30:16 -0800

    Implemented REST api to get team info

--- aws/web/data/getData.js
+++ aws/web/data/getData.js
@@ -1,6 +1,16 @@
+var fakeData = [
+    {},
+    {lat: 38.5818756, lng: -121.493181},
+    {lat: 38.5518551, lng: -121.491181},
+    {lat: 38.5972452, lng: -121.446315},
+    {lat: 38.5671344, lng: -121.454877},
+    {lat: 38.6251146, lng: -121.461503},   
+];
+
 function getFirefighterData(id, datetime){
     var result = {   
             datetime: datetime,
+            status: 'Active',
             temp: (Math.random() > 0.5 ? 1 : -1) * Math.random()/10 + 100,
             hum: (Math.random() > 0.5 ? 1 : -1) * Math.random()/10 + 50,
         };
@@ -16,6 +26,30 @@ function getFirefighterDataHistory(id, datetime, count){
     return result;
 }
 
+var noOfTeams = 5;
+
+function getTeams(){
+    var result = []
+    for (var i = 0; i < noOfTeams; i++){
+        result.push(getTeamInfo(i + 1))
+    }
+    return result;
+}
+
+function getTeamInfo(id){
+    var result = {
+        id: id,
+        name: "Team " + id,
+        pos: {
+            lat: fakeData[id].lat + Math.random()/5000,
+            lng: fakeData[id].lng +  Math.random()/5000
+        },
+        status: 'Deployed',
+        membersId: ['iotDevice' + id + '1', 'iotDevice' + id + '2']
+    }
+    return result;
+}
 
 module.exports.getFirefighterData = getFirefighterData;
 module.exports.getFirefighterDataHistory = getFirefighterDataHistory;
+module.exports.getTeams = getTeams;
