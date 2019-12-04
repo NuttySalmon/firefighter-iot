e21c96044266b5a88b7b06c703ff9d026c68ccc1
NuttySalmon <nuttysalmon@gmail.com>
2019-12-04 01:49:06 -0800

    Implemented multithreaded reconnection code and publishing data to MQTT topic

--- linux_iot/run.py
+++ linux_iot/run.py
@@ -1,6 +1,12 @@
 from aws_iot_client import AWSIOTClient
 from sensor import Sensor
+import time
+from datetime import datetime
+import threading
 import json
+from concurrent.futures import ProcessPoolExecutor as executor
+
+
 class Config:
     def __init__(self, host, rootCA, cert, privkey, clientId, devices):
         self.host = host
@@ -10,15 +16,60 @@ class Config:
         self.clientId = clientId
         self.devices = devices
 
+def connect_all(devices):
+    for device in devices:
+        thread = threading.Thread(target=connect, args=(device,))
+        thread.run()
+
+   
+def connect(device):
+    lock = device.lock
+    if not lock.locked():
+        with lock:
+            if not device.connected:
+                device.connect()
+
+def publish(devices, client):
+    while True:
+        data = []
+        for device in devices:
+            data.append(device.get_data())            
+
+        payload = {
+            "clientId": client.config.clientId,  
+            "datetime": datetime.now().strftime("%Y%m%d%H%M%S"),
+            "status": "Deployed",
+            "members": data
+        }
+        client.publish(payload) 
+        time.sleep(1)
+        
 
 def main():
-	config_json = open('./config.json')
-	config = Config(**json.load(config_json))
-	client = AWSIOTClient(config)
-	client.publish({'message': 'test'})
+    config_json = open('./config.json')
+    config = Config(**json.load(config_json))
+    client = AWSIOTClient(config)
+    devices = []
 
+    for info in config.devices:
+        device = Sensor(info['addr'], info['deviceId'])
+        devices.append(device)
+    
+        connect_thread = threading.Thread(target=connect_all, args=(devices,))
+        publish_thread = threading.Thread(target=publish, args=(devices, client))
+
+        connect_thread.start()
+        publish_thread.start()
+
+    
+    for t in threading.enumerate():
+        if t is not threading.current_thread():
+            t.join()
+
+                       
 if __name__ == '__main__':
 	main()
+
 
 
 
